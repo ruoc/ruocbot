544f0b5a343f9e09526309bc714418d44940d7b7
Ruoc <ngoc.rock@gmail.com>
2019-08-21 14:00:09 +0700

    thêm command /gai

--- bot.js
+++ bot.js
@@ -1,4 +1,8 @@
 
+const dotenv = require('dotenv');
+const Extra = require('telegraf/extra')
+const fs = require('fs')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.use((ctx, next) => {
@@ -8,6 +12,9 @@ bot.use((ctx, next) => {
     console.log('Response time %sms', ms)
   })
 })
+
+
+bot.command('gai', (ctx) => ctx.reply('Ok gai');
 
 bot.on('text', (ctx) => ctx.reply('Hello World'))
 bot.launch()
