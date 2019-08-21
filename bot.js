3d9eaeb8f99684625fb48eb5ac552cdbc29cf5f4
Ruoc <ngoc.rock@gmail.com>
2019-08-21 13:46:30 +0700

    init bot

---
+++ bot.js
@@ -0,0 +1,12 @@
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.use((ctx, next) => {
+  const start = new Date()
+  return next(ctx).then(() => {
+    const ms = new Date() - start
+    console.log('Response time %sms', ms)
+  })
+})
+
+bot.on('text', (ctx) => ctx.reply('Hello World'))
+bot.launch()
