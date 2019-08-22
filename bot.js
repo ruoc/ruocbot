c39cafa47da7a01abac8b58250a746832dd81379
Ruoc <ngoc.rock@gmail.com>
2019-08-22 11:40:40 +0700

    test reddit get

--- bot.js
+++ bot.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const Extra = require('telegraf/extra')
 const fs = require('fs')
 const Telegraf = require('telegraf')
+const axious = require('axious')
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
+const redditUrl = 'https://www.reddit.com/r/AsiansGoneWild/random.json';
 bot.use((ctx, next) => {
   const start = new Date()
   return next(ctx).then(() => {
@@ -13,8 +14,13 @@ bot.use((ctx, next) => {
   })
 })
 
+function getRandomPostMedia(){
+	axious.get(redditUrl)
+	.then(function(res){
+		console.log(JSON.parse(res))
+	})
+}
 
-bot.command('gai', (ctx) => ctx.reply('Ok gai'))
+bot.command('gai', (ctx) => getRandomPostMedia())
 
-bot.on('text', (ctx) => ctx.reply('Hello World'))
 bot.launch()
