5466df550595730898a4d975057437752d39227c
root <root@ruoc.xyz>
2019-09-06 06:51:42 +0000

    update bot

--- bot.js
+++ bot.js
@@ -5,7 +5,9 @@ const fs = require('fs')
 const Telegraf = require('telegraf')
 const axios = require('axios')
 const bot = new Telegraf(process.env.BOT_TOKEN)
-const redditUrl = 'https://www.reddit.com/r/AsiansGoneWild/random.json';
+const cuteGif = 'https://www.reddit.com/r/asiangirlsbeingcute/random.json';
+const sotiGif = 'https://www.reddit.com/r/AsiansGoneWild/random.json';
+//boobbounce
 bot.use((ctx, next) => {
   const start = new Date()
   return next(ctx).then(() => {
@@ -14,14 +16,25 @@ bot.use((ctx, next) => {
   })
 })
 
-function getRandomPostMedia(){
+function getRandomPostMedia(ctx,url){
 	var result = {type:"text"}
-	axios.get(redditUrl)
+	axios.get(url)
 	.then(function(res){
-		console.log(res.data[0].data.children.url)
+		try{
+			if(typeof(res.data[0].data.children[0].data.media.oembed) !== 'undefined'){
+				if(res.data[0].data.children[0].data.media.oembed.thumbnail_url.indexOf('.gif') > -1){
+					ctx.replyWithAnimation(res.data[0].data.children[0].data.media.oembed.thumbnail_url)
+				}else{
+					ctx.replyWithPhoto(res.data[0].data.children[0].data.media.oembed.thumbnail_url);
+				}
+			}
+		}catch(e){
+			ctx.replyWithPhoto(res.data[0].data.children[0].data.url)
+		}
+	//	ctx.reply(res.data[0].data.children[0].data.url)
 	})
 }
 
-bot.command('gai', (ctx) => getRandomPostMedia())
-
+bot.command('gai', (ctx) => getRandomPostMedia(ctx,cuteGif))
+bot.command('soti', (ctx) => getRandomPostMedia(ctx,sotiGif))
 bot.launch()
