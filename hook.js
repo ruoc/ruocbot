ef1e781f9bd4d61c701a69f135fe262169e16f8e
Rock <rock.n@shopstack.asia>
2020-07-09 15:14:40 +0700

    change webhook

--- hook.js
+++ hook.js
@@ -1,2 +1,36 @@
-var hookshot = require('hookshot');
-hookshot('refs/heads/master', 'git pull && pm2 restart bot').listen(3000)
+require('dotenv').config();
+import http from 'http';
+import crypto from 'crypto';
+import { exec } from 'child_process';
+ 
+const GITHUB_REPOSITORIES_TO_DIR = {
+  'ruoc/ruocbot': '/root/server/ruocbot'
+};
+ 
+http
+  .createServer((req, res) => {
+    req.on('data', chunk => {
+      const signature = `sha1=${crypto
+        .createHmac('sha1', process.env.GITHUB_SECRET)
+        .update(chunk)
+        .digest('hex')}`;
+ 
+      const isAllowed = req.headers['x-hub-signature'] === signature;
+ 
+      const body = JSON.parse(chunk);
+ 
+      const isMaster = body?.ref === 'refs/heads/master';
+      const directory = GITHUB_REPOSITORIES_TO_DIR[body?.repository?.full_name];
+ 
+      if (isAllowed && isMaster && directory) {
+        try {
+          exec(`cd ${directory} && pm2 restart bot`);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+ 
+    res.end();
+  })
+  .listen(8080);
