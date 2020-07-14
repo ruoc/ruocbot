d127b512d9a5009797c867de3659e8a37216b877
Rock <rock.n@shopstack.asia>
2020-07-14 11:32:27 +0700

    a

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ require('dotenv').config();
 const Telegraf = require('telegraf');
 const app = new Telegraf(process.env.BOT_TOKEN);
 
+function filterUsername(names){
+    var copyItems = []
+    // before
+    for (let i = 0; i < names.length; i++) {
+        if(names[i].includes('@')) copyItems.push(names[i])
+    }
+
+}
+
 app.hears('hi', ctx => {
   return ctx.reply('Hey!');
 });
+
 app.hears(/^.*(\/check|tín|Check|Tín|check|Check|Tin|tin).*$/, ctx => {
     var userName = ctx.message.text.match(/@([\w]+)/);
-    console.log(userName);
+    console.log(filterUsername(userName));
     if(userName){
         return ctx.reply(userName.join(' '));
     }
