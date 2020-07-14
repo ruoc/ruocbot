d148edcfa7460e9db8b3fb2622feec67e3537ca7
Rock <rock.n@shopstack.asia>
2020-07-14 10:59:22 +0700

    f

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.hears('hi', ctx => {
   return ctx.reply('Hey!');
 });
 app.hears(/^.*(\/check|tín|Check|Tín|check|Check|Tin|tin).*$/, ctx => {
-    //var userName = ctx.message.match(/^@?([a-zA-Z0-9_]){1,15}$/);
+    var userName = ctx.message.text.match(/^@?([a-zA-Z0-9_]){1,15}$/);
     console.log(ctx.message);
-    return ctx.match;
+    return ctx.reply(userName.join(' '));
 });
 app.startPolling();
