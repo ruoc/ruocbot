0521fd9a3edd6f6a45fb991faa0ac04d38f42e23
Rock <rock.n@shopstack.asia>
2020-07-14 13:39:44 +0700

    a

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 require('dotenv').config();
 const Telegraf = require('telegraf');
+const mongoose = require('mongoose');
 const app = new Telegraf(process.env.BOT_TOKEN);
+
+mongoose.connect('mongodb://localhost/moonotes');
+const Schema = mongoose.Schema;
+
+const dealerSchema = new Schema({
+    user_id: Number,
+    username: String,
+    information: String
+});
+
+const Dealer = mongoose.model('Dealer', dealerSchema);
+
+bot.command('/add', ctx => {
+    const message = ctx.message;
+    const myDealer = new Dealer({
+      name: message.text.split(';\n')[0].replace('/add ', ''),
+      description: message.text.split(';\n')[1],
+      type: message.text.split(';\n')[2].toLowerCase(),
+      author: message.from.id,
+    });
+  
+    myNote.save((err, mess) => {
+      ctx.reply('Saved! ✅');
+    });
+});
 
 function filterUsername(names){
     var copyItems = []
