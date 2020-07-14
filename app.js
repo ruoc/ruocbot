881d9a1e64b52324b13061f9e06cc7e29f1486e8
Rock <rock.n@shopstack.asia>
2020-07-14 10:41:27 +0700

    Fix bot hear

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.hears('hi', ctx => {
   return ctx.reply('Hey!');
 });
 app.hears(/^.*(\/check|tín|Check|Tín|check|Check|Tin|tin).*$/gm, ctx => {
-    var userName = ctx.message.match(/^@?([a-zA-Z0-9_]){1,15}$/);
+    //var userName = ctx.message.match(/^@?([a-zA-Z0-9_]){1,15}$/);
     return ctx.match.join(' ');
 });
 app.startPolling();
