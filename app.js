51c7e0ff2778637fffc0f330f8aa54db1822adf2
Rock <rock.n@shopstack.asia>
2020-07-14 10:35:53 +0700

    test match username

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const app = new Telegraf(process.env.BOT_TOKEN);
 app.hears('hi', ctx => {
   return ctx.reply('Hey!');
 });
-
+app.hears(/^.*(\/check|tín|Check|Tín|check|Check|Tin|tin).*$/gm, ctx => {
+    var userName = ctx.message.match(/^@?([a-zA-Z0-9_]){1,15}$/);
+    return userName.join(' ');
+});
 app.startPolling();
